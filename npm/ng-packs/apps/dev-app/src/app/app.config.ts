e0ee425bc5ce8ac62fddfaa05b4af421750e9dfe
erdemcaygor <erdem.caygor@gmail.com>
2025-05-09 16:25:55 +0300

    ssr config

--- npm/ng-packs/apps/dev-app/src/app/app.config.ts
+++ npm/ng-packs/apps/dev-app/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { appRoutes } from './app.routes';
 import { APP_ROUTE_PROVIDER } from './route.provider';
@@ -35,5 +36,6 @@ export const appConfig: ApplicationConfig = {
     provideFeatureManagementConfig(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideClientHydration(withEventReplay()),
   ],
 };

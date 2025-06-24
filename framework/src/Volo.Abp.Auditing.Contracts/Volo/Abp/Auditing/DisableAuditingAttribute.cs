e770f89548e432c925c0f8ca22cc253a7d4f2db0
maliming <malimings@gmail.com>
2025-06-24 21:19:50 +0800

    Refactor `DisableAuditingAttribute` to allow it to ignore changes to `AuditProperties`.
    Resolve #23168

--- framework/src/Volo.Abp.Auditing.Contracts/Volo/Abp/Auditing/DisableAuditingAttribute.cs
+++ framework/src/Volo.Abp.Auditing.Contracts/Volo/Abp/Auditing/DisableAuditingAttribute.cs
@@ -5,5 +5,7 @@ namespace Volo.Abp.Auditing;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
 public class DisableAuditingAttribute : Attribute
 {
+    public bool UpdateModificationProps { get; set; } = true;
 
+    public bool PublishEntityEvent { get; set; } = true;
 }

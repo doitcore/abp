1e45dbd5e1ab37e42c1a59573145ae3098e240d4
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2025-08-05 11:31:48 +0300

    refactor

--- modules/blogging/src/Volo.Blogging.Admin.Application/Volo/Blogging/Admin/BloggingAdminApplicationModule.cs
+++ modules/blogging/src/Volo.Blogging.Admin.Application/Volo/Blogging/Admin/BloggingAdminApplicationModule.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Application;
 using Volo.Abp.Mapperly;
 using Volo.Abp.Caching;
 using Volo.Abp.Modularity;
-using Volo.Blogging.Comments;
-using Volo.Blogging.Posts;
 
 namespace Volo.Blogging.Admin
 {
--- modules/blogging/src/Volo.Blogging.Domain/Volo/Blogging/Tagging/Tag.cs
+++ modules/blogging/src/Volo.Blogging.Domain/Volo/Blogging/Tagging/Tag.cs
@@ -15,7 +15,7 @@ namespace Volo.Blogging.Tagging
 
         public virtual int UsageCount { get; protected internal set; }
 
-        public Tag()
+        protected Tag()
         {
 
         }

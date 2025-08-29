d2cb3dad71b60b4f7b212f189dfd5c9d99f9c72f
maliming <malimings@gmail.com>
2025-08-29 17:30:46 +0800

    Update Mapperly mapper placeholders in templates

--- templates/app-nolayers/aspnet-core/MyCompanyName.MyProjectName.Blazor.WebAssembly/Server/ObjectMapping/MyProjectNameMappers.cs
+++ templates/app-nolayers/aspnet-core/MyCompanyName.MyProjectName.Blazor.WebAssembly/Server/ObjectMapping/MyProjectNameMappers.cs
@@ -3,8 +3,12 @@ using Volo.Abp.Mapperly;
 
 namespace MyCompanyName.MyProjectName.ObjectMapping;
 
-[Mapper]
-public partial class MyProjectNameMappers
-{
-    /* Create your Mapperly object mappings here */
-}
+// This file is a placeholder for Mapperly mappers.
+// Add your mapper classes here following the pattern:
+// [Mapper]
+// public partial class SourceToDestinationMapper : MapperBase<Source, Destination>
+// {
+//     public override partial Destination Map(Source source);
+//     public override partial void Map(Source source, Destination destination);
+// }
+

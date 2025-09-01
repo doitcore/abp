3f41906037d0475ba9b484376e77e60004066802
Fahri Gedik <fahrican9978@gmail.com>
2025-09-01 17:13:43 +0300

    Revert "Improve column width handling in ExtensibleTable"
    This reverts commit 53cfc6a81779c7e30b7bbfd1425940180dfa74a8.

--- npm/ng-packs/packages/components/extensible/src/lib/components/extensible-table/extensible-table.component.ts
+++ npm/ng-packs/packages/components/extensible/src/lib/components/extensible-table/extensible-table.component.ts
@@ -212,11 +212,6 @@ export class ExtensibleTableComponent<R = any> implements OnChanges, AfterViewIn
 
       return record;
     });
-
-    if ((this.columnWidths as any)?.some?.((w: number | undefined) => w == null)) {
-      this.setColumnWidths(this.columnWidths?.[0] as any);
-      this.cdr.markForCheck();
-    }
   }
 
   isVisibleActions(rowData: any): boolean {

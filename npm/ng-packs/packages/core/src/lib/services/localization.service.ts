4cfd4abea1b64821d8070d872d7f29e7fbf71ac2
sumeyye <sum.the.developer@gmail.com>
2025-09-08 13:40:15 +0300

    fix: use acc parameter to merge resources instead

--- npm/ng-packs/packages/core/src/lib/services/localization.service.ts
+++ npm/ng-packs/packages/core/src/lib/services/localization.service.ts
@@ -280,7 +280,7 @@ function recursivelyMergeBaseResources(
 
   return item.baseResources.reduce((acc, baseResource) => {
     const baseItem = recursivelyMergeBaseResources(baseResource, source);
-    const texts = { ...baseItem.texts, ...item.texts };
+    const texts = { ...baseItem.texts, ...acc.texts };
     return { ...acc, texts };
   }, item);
 }

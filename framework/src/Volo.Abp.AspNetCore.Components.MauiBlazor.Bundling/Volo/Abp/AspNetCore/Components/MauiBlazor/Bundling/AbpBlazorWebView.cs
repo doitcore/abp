4006d0e00c335a8270cac6cb8f058289f13f9867
maliming <malimings@gmail.com>
2025-10-13 14:40:24 +0800

    Revert "Initialize timezone service in AbpBlazorWebView"
    This reverts commit f9c93a1f6d89a1710b1073bd65b343fe4c04c264.

--- framework/src/Volo.Abp.AspNetCore.Components.MauiBlazor.Bundling/Volo/Abp/AspNetCore/Components/MauiBlazor/Bundling/AbpBlazorWebView.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.MauiBlazor.Bundling/Volo/Abp/AspNetCore/Components/MauiBlazor/Bundling/AbpBlazorWebView.cs
@@ -1,20 +1,11 @@
 using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Maui;
-using Volo.Abp.Threading;
 
 namespace Volo.Abp.AspNetCore.Components.MauiBlazor.Bundling;
 
 public class AbpBlazorWebView : BlazorWebView
 {
-    public AbpBlazorWebView()
-    {
-        BlazorWebViewInitialized += (s, e) =>
-        {
-            AsyncHelper.RunSync(() => Handler!.GetRequiredService<MauiBlazorCurrentTimezoneService>().InitializeAsync());
-        };
-    }
-
     public override IFileProvider CreateFileProvider(string contentRootDir)
     {
         return new CompositeFileProvider(Handler!.GetRequiredService<IMauiBlazorContentFileProvider>(), base.CreateFileProvider(contentRootDir));

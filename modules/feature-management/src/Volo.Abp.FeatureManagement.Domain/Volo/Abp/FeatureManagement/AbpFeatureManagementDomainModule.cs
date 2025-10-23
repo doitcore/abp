f816227cc52b226233600b1f8effb49032c8f0ea
maliming <malimings@gmail.com>
2025-10-23 10:03:01 +0800

    Add configuration-based feature value provider

--- modules/feature-management/src/Volo.Abp.FeatureManagement.Domain/Volo/Abp/FeatureManagement/AbpFeatureManagementDomainModule.cs
+++ modules/feature-management/src/Volo.Abp.FeatureManagement.Domain/Volo/Abp/FeatureManagement/AbpFeatureManagementDomainModule.cs
@@ -29,6 +29,7 @@ public class AbpFeatureManagementDomainModule : AbpModule
         Configure<FeatureManagementOptions>(options =>
         {
             options.Providers.Add<DefaultValueFeatureManagementProvider>();
+            options.Providers.Add<ConfigurationFeatureManagementProvider>();
             options.Providers.Add<EditionFeatureManagementProvider>();
 
             //TODO: Should be moved to the Tenant Management module

e662b62ec308deadf403eb13557152050e5713f4
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2025-11-10 14:35:48 +0300

    Create IResourcePermissionManager.cs

---
+++ modules/permission-management/src/Volo.Abp.PermissionManagement.Domain/Volo/Abp/PermissionManagement/IResourcePermissionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Volo.Abp.PermissionManagement;
+
+public interface IResourcePermissionManager
+{
+    Task<PermissionWithGrantedProviders> GetAsync(
+        string permissionName,
+        string providerName,
+        string providerKey,
+        string resourceName,
+        string resourceKey
+    );
+    
+    Task<MultiplePermissionWithGrantedProviders> GetAsync(
+        string[] permissionNames,
+        string provideName,
+        string providerKey,
+        string resourceName,
+        string resourceKey
+    );
+    
+    Task<List<PermissionWithGrantedProviders>> GetAllAsync(
+        string providerName,
+        string providerKey,
+        string resourceName,
+        string resourceKey
+    );
+    
+    Task SetAsync(
+        string permissionName,
+        string providerName,
+        string providerKey,
+        string resourceName,
+        string resourceKey,
+        bool isGranted
+    );
+}

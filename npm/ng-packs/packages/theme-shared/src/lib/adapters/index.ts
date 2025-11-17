212290bf5f4b27588d9a799eb361991ebc7b2f26
Fahri Gedik <fahrican9978@gmail.com>
2025-11-17 12:38:27 +0300

    Add custom i18n adapter for ng-bootstrap datepicker
    Introduced DatepickerI18nAdapter to provide localized labels for the ng-bootstrap datepicker using Angular's locale and ABP config state. Registered the adapter as the provider for NgbDatepickerI18n and updated the adapters index export.

--- npm/ng-packs/packages/theme-shared/src/lib/adapters/index.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/adapters/index.ts
@@ -1,3 +1,4 @@
 export * from './date-time.adapter';
 export * from './date.adapter';
+export * from './datepicker-i18n.adapter';
 export * from './time.adapter';
--- npm/ng-packs/packages/theme-shared/src/lib/providers/ng-bootstrap-config.provider.ts
+++ npm/ng-packs/packages/theme-shared/src/lib/providers/ng-bootstrap-config.provider.ts
@@ -1,7 +1,12 @@
 import { inject, provideAppInitializer } from '@angular/core';
-import { NgbInputDatepickerConfig, NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerI18n, NgbInputDatepickerConfig, NgbTypeaheadConfig } from '@ng-bootstrap/ng-bootstrap';
+import { DatepickerI18nAdapter } from '../adapters';
 
 export const NG_BOOTSTRAP_CONFIG_PROVIDERS = [
+  {
+    provide: NgbDatepickerI18n,
+    useClass: DatepickerI18nAdapter,
+  },
   provideAppInitializer(() => {
     configureNgBootstrap();
   }),

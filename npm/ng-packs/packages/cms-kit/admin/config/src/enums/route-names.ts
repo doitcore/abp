2e61b5d31e89fc3aa56a6e79cd0b750b76daf751
sumeyye <sum.the.developer@gmail.com>
2025-11-19 13:38:36 +0300

    update: route names for the admin cms kit

--- npm/ng-packs/packages/cms-kit/admin/config/src/enums/route-names.ts
+++ npm/ng-packs/packages/cms-kit/admin/config/src/enums/route-names.ts
@@ -1,10 +1,10 @@
 export enum eCmsKitAdminRouteNames {
   Cms = 'CmsKit::Menu:CMS',
   Comments = 'CmsKit::CmsKit.Comments',
-  Tags = 'CmsKit::Menu:Tags',
-  Pages = 'CmsKit::Menu:Pages',
-  Blogs = 'CmsKit::Menu:Blogs',
-  BlogPosts = 'CmsKit::Menu:BlogPosts',
-  Menus = 'CmsKit::Menu:Menus',
-  GlobalResources = 'CmsKit::Menu:GlobalResources',
+  Tags = 'CmsKit::CmsKit.Tags',
+  Pages = 'CmsKit::Pages',
+  Blogs = 'CmsKit::Blogs',
+  BlogPosts = 'CmsKit::BlogPosts',
+  Menus = 'CmsKit::Menus',
+  GlobalResources = 'CmsKit::GlobalResources',
 }

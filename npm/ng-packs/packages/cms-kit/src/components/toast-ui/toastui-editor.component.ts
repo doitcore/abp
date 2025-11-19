0c65999d13b21039590d60d4ca5ebbc9e748e1ba
sumeyye <sum.the.developer@gmail.com>
2025-11-19 13:38:15 +0300

    add: common code mirror and toast ui components

---
+++ npm/ng-packs/packages/cms-kit/src/components/toast-ui/toastui-editor.component.ts
@@ -0,0 +1,86 @@
+import { AbpLocalStorageService } from '@abp/ng.core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  forwardRef,
+  inject,
+  DOCUMENT,
+  PLATFORM_ID,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import Editor from '@toast-ui/editor';
+
+@Component({
+  selector: 'abp-toastui-editor',
+  template: `<div #editorContainer></div>`,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ToastuiEditorComponent),
+      multi: true,
+    },
+  ],
+})
+export class ToastuiEditorComponent implements AfterViewInit, ControlValueAccessor {
+  @ViewChild('editorContainer', { static: true })
+  editorContainer!: ElementRef<HTMLDivElement>;
+
+  private editor!: Editor;
+  private value = '';
+
+  private document = inject(DOCUMENT);
+  private platformId = inject(PLATFORM_ID);
+  private localStorageService = inject(AbpLocalStorageService);
+
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value || '';
+    if (this.editor) {
+      this.editor.setMarkdown(this.value);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    this.initializeEditor();
+    // TODO: Revise this approach on lepton side :: Service injection through DI
+    // this.setupThemeListener();
+  }
+
+  private getTheme(): string {
+    return this.localStorageService.getItem('LPX_THEME') || 'light';
+  }
+
+  private initializeEditor(): void {
+    const theme = this.getTheme();
+    this.editor = new Editor({
+      el: this.editorContainer.nativeElement,
+      previewStyle: 'tab',
+      height: '500px',
+      autofocus: true,
+      theme: theme,
+      initialValue: this.value,
+    });
+
+    this.editor.addHook('change', () => {
+      const value = this.editor.getMarkdown();
+      this.onChange(value);
+    });
+  }
+}

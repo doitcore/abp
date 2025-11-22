8fedeb71667f112f4a9025d99ee79ad7a99ff8b9
maliming <6908465+maliming@users.noreply.github.com>
2025-11-22 15:50:17 +0800

    Refactor Swagger security requirement setup

--- modules/openiddict/app/OpenIddict.Demo.API/Program.cs
+++ modules/openiddict/app/OpenIddict.Demo.API/Program.cs
@@ -42,19 +42,9 @@ builder.Services.AddSwaggerGen(options =>
         }
     });
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement()
     {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "oauth2"
-                }
-            },
-            Array.Empty<string>()
-        }
+        [new OpenApiSecuritySchemeReference("oauth2", document)] = []
     });
 });
 

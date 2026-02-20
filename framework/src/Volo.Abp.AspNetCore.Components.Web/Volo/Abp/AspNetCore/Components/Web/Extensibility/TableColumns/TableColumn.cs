f212a639e1ff62077613419d3a7dd6f73e465dcc
maliming <malimings@gmail.com>
2026-02-20 16:46:56 +0800

    Refactor TableColumn width property to string type and implement BlaoriseFluentSizingParse for dynamic sizing in DataGrid columns

--- framework/src/Volo.Abp.AspNetCore.Components.Web/Volo/Abp/AspNetCore/Components/Web/Extensibility/TableColumns/TableColumn.cs
+++ framework/src/Volo.Abp.AspNetCore.Components.Web/Volo/Abp/AspNetCore/Components/Web/Extensibility/TableColumns/TableColumn.cs
@@ -11,10 +11,7 @@ public class TableColumn
 
     public string Data { get; set; } = default!;
 
-    /// <summary>
-    /// px
-    /// </summary>
-    public double Width { get; set; } = default!;
+    public string Width { get; set; } = default!;
 
     public string PropertyName { get; set; } = default!;
 

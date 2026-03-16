f0228f007aeb090e8d00ae2c16371b8601e3aeea
maliming <malimings@gmail.com>
2026-03-16 10:04:35 +0800

    Add AddFunction helper to AbpTickerQFunctionProvider and use named tuple fields

--- framework/src/Volo.Abp.TickerQ/Volo/Abp/TickerQ/AbpTickerQFunctionProvider.cs
+++ framework/src/Volo.Abp.TickerQ/Volo/Abp/TickerQ/AbpTickerQFunctionProvider.cs
@@ -8,13 +8,22 @@ namespace Volo.Abp.TickerQ;
 
 public class AbpTickerQFunctionProvider : ISingletonDependency
 {
-    public Dictionary<string, (string, TickerTaskPriority, TickerFunctionDelegate, int)> Functions { get;}
+    public Dictionary<string, (string CronExpression, TickerTaskPriority Priority, TickerFunctionDelegate Function, int MaxConcurrency)> Functions { get; }
 
-    public Dictionary<string, (string, Type)> RequestTypes { get; }
+    public Dictionary<string, (string TypeName, Type Type)> RequestTypes { get; }
 
     public AbpTickerQFunctionProvider()
     {
         Functions = new Dictionary<string, (string, TickerTaskPriority, TickerFunctionDelegate, int)>();
         RequestTypes = new Dictionary<string, (string, Type)>();
+    }
+
+    public void AddFunction(
+        string name,
+        TickerFunctionDelegate function,
+        TickerTaskPriority priority = TickerTaskPriority.Normal,
+        int maxConcurrency = 0)
+    {
+        Functions.TryAdd(name, (string.Empty, priority, function, maxConcurrency));
     }
 }
